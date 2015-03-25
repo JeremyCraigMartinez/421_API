dd40f126491378c62638dfc15f2f438c84830e16
JeremyCraigMartinez <jeremymartinez11@gmail.com>
2015-03-24 23:10:22 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+###This is the API for the Senior Design Wearable Sensors Project
+
+> IP Address: 104.236.169.12  
+> Port: 5024
+
+This server is SSL encrypted. Therefore, it is acceptable through HTTPS ONLY!!!!! 
+
+Test it in your browser right now. Access our API [here](https://104.236.169.12:5024). 
+
+For now, there is nothing guarding the information attainble through the API calls. However, when in development later on, There will be an API key and a proxy server. This proxy server will check that the GET/POST request contains the API key. It will ONLY accept POST request data through a JSON request. 
+
+It will ```urlencode``` each request to avoid any injection attack. It will then look for the API key. If the user is authenticated, the proxy will route the clients request to API server. The API server will only accept request from the IP address that the proxy server is on. This is clearly for security.
+
+###API Calls include:
+
+     | PATH       |
+-----|------------|
+GET  | /          |
+-----|------------|
---
+++ index.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
+var passport = require('passport');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser());
+/* server deployment */
+/*
+var https = require('https');
+var options = {
+  key: fs.readFileSync('ssl/server.key'),
+  cert: fs.readFileSync('ssl/server.crt'),
+  ca: fs.readFileSync('ssl/ca.crt'),
+  requestCert: false,
+  rejectUnauthorized: false
+};
+var secureServer = https.createServer(options, app).listen('5024', function(){
+  //console.log("Secure Express server listening on port 5024");
+});
+/* end server deployment */
+
+var http = require('http');
+var http = require('http').Server(app);
+http.listen('5024');
+
+/* API calls */
+app.get('/',function(req,res,next){
+  res.end("API is working on port 5024 - WSU Senior Design - Wearable Sensors. \nCURRENT USER: " + user + " PASSWORD: " + password + " DOCTOR: " + doctor);
+});
+
+app.get('/login', function(req,res,next){
+  res.end('you need to send a POST request to receive an OAuth2 access token');
+});
+
+var user;
+var password;
+var doctor;
+app.post('/signup', function(req,res,next){
+  obj = req.body;//JSON.parse(req.body);
+  console.log(obj);
+  user = obj['username'];
+  password = obj['password'];
+  doctor = obj['doctor'];
+  res.end('thank you');
+});
