2df97204bf5ec41bd707a55b1f3cb0904daaad80
jeremy <jeremymartinez11@gmail.com>
2015-03-30 14:51:39 -0700

    updated gitignore and added config file and db.js which is not being used currently

---
+++ config.sh
@@ -0,0 +1,9 @@
+#mongo installation
+wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1404-3.0.1.tgz
+tar xvf mongodb-linux-x86_64-ubuntu1404-3.0.1
+
+sudo su -
+cd /home/jeremy/mongodb-linux-x86_64-ubuntu1404-3.0.1/bin/
+cp * /usr/local/bin
+exit
+
