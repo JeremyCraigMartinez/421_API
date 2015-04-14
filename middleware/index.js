13ce9df8ed980523af7778f16cdbe38f3a2126c0
jeremy <jeremymartinez11@gmail.com>
2015-04-14 01:03:06 -0700

    cors installed and used in middleware/index.js

--- middleware/index.js
+++ middleware/index.js
@@ -4,10 +4,13 @@ var fs = require('fs');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var bodyParser = require('body-parser');
+var cors = require('cors')
 
 module.exports = function (app) {
 	// create a write stream (in append mode)
 	var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'})
+
+	app.use(cors());
 
 	// setup the logger
 	app.use(morgan('dev', {stream: accessLogStream}))
