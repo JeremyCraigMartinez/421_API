f7b70f0dded17ff6d05e25bd1a898fda7b55ed99
jeremy <jeremymartinez11@gmail.com>
2015-07-02 11:13:45 -0700

    test bug

--- helpers/validate/groups.js
+++ helpers/validate/groups.js
@@ -1,7 +1,11 @@
 var Groups = require('../../models/Groups');
 module.exports = function (groups, done) {
 	var all = [];
-	if (typeof groups === "string") groups = [groups];
+	if (typeof groups === "string") {
+		if (groups === "")
+			done(true);
+		groups = [groups];
+	}
 
 	Groups.find(function (err, data) {
 		for (var key in data) all.push(data[key]._id);
